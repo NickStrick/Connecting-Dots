a71d02baaea721798d2c7e1eec706ae85500bfd3
Nick Stricker <strickerdev@gmail.com>
2026-04-10 18:34:04 -0500

    temp fix for fellows

--- src/app/components/FellowsComponent.tsx
+++ src/app/components/FellowsComponent.tsx
@@ -147,15 +147,16 @@ export default function Fellows({
             >
               {/* Avatar */}
               <div className={`mb-4 ${align === 'center' ? 'mx-auto' : ''}`}>
-                {fellow.avatarUrl ? (
+                {false ? (
                   <div className={`relative ${align === 'center' ? 'mx-auto' : ''} w-[150px] h-[150px] rounded-full overflow-hidden`}>
-                    <Image
-                      src={fellow.avatarUrl}
-                      alt={fellow.name}
-                      width={150}
-                      height={150}
-                      className="w-full h-full object-cover"
-                    />
+                     <Image
+    src={fellow.avatarUrl}
+    alt={fellow.name}
+    fill
+    className="object-cover"
+    sizes="150px"
+    unoptimized
+  />
                   </div>
                 ) : (
                   <div
